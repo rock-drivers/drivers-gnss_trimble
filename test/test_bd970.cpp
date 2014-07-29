94e780aa4afc5b910ad3f079eaa293d6a57ed87b
Javier Hidalgo Carrio <javier.hidalgo_carrio@dfki.de>
2014-07-29 20:24:25 +0200

    added open in teh test driver

--- test/test_bd970.cpp
+++ test/test_bd970.cpp
@@ -7,6 +7,7 @@ BOOST_AUTO_TEST_CASE(it_should_not_crash_when_welcome_is_called)
 {
     bd970::Bd970 mybd970(1024, 1.0);
     mybd970.welcome();
+    mybd970.open("/dev/ttyXR0", iodrivers_base::Driver::SERIAL_921600);
 
     while (100)
     {
